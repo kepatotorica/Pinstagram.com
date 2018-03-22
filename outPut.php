cfab1ebc8894dc89172c2e4794e1f5e0b7ab0762
kepatoto <kepatoto@gmail.com>
2018-03-21 20:51:04 -0600

    attempting to make outPut.php run

--- outPut.php
+++ outPut.php
@@ -7,13 +7,14 @@ $dao->saveUser($_POST["email"],$_POST["username"],$_POST["password"]);
 // echo '<br><br><br><br><br>saved users<br>';
 $users = $dao->getUsers();
 echo count($users) . '<br><br><br>';
-for( $i = 0; $i<count($users); $i++ ) {
-  echo $users[$i]['user_email'] . '<br>';
-   }
+// for( $i = 0; $i<count($users); $i++ ) {
+  // echo $users[$i]['user_email'] . '<br>';
+   // }
 // foreach( $users as $value]) {
 //   echo 'a';
 // }
-echo("<pre>" . print_r($users,1) . "</pre>");
+echo 'most recently added user';
+echo("<pre>" . print_r($users[count($users) - 1],1) . "</pre>");
 
 $email = $users[1]['user_email'];
 // <li><a href="#" class="loc_button glow_loc">Jen</a></li>
@@ -26,14 +27,16 @@ $email = $users[1]['user_email'];
 
 <?php
     for( $i = 0; $i<count($users); $i++ ) {
-      echo '<li><a href="#" class="loc_button glow_loc">' . $users[$i]['user_email']  . '</a></li><li>';
+      echo '<a href="#" class="loc_button glow_loc">' . $users[$i]['user_email']  . '</a><br>';
   }
 ?>
 <!-- Your name: <?php //echo $_POST["username"]; ?><br> -->
 <!-- Your email: <?php //echo $_POST["email"]; ?><br> -->
 <!-- Your pass: <?php //echo $_POST["password"]; ?><br> -->
 
-<!-- <?php //echo $email . 'we could still ref it'; ?> -->
+<?php
+ //echo $email . 'we could still ref it';
+ ?>
 
 <form name="form" action="/index.php" method="POST">
     <input type="submit" class="submit" value="Register">
