9603da716c147d28227a683173155c96f3b13f87
kepatoto <kepatoto@gmail.com>
2018-04-15 18:10:57 -0600

    formatting

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 <?php
 echo '<div class="spacedFooter">
     <span>&copy Kepa Totorica</span>
+    <span>Pinstagram</span>
     <span>(208)599-5425</span>
     </div>';
-
 ?>
+<span><img id="spanImg" src="images/myFlavicon.png" alt="logo"></span>
