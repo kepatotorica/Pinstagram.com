fc6c7953166e7c1088c902124d89e4d339577387
kepatoto <kepatoto@gmail.com>
2018-04-16 18:05:07 -0600

    added an edit option

--- picDisplay.php
+++ picDisplay.php
@@ -48,8 +48,8 @@ $edit = false;
 <div class="gallery">
   <?php
 
-  echo '<div class="desc"><pre>'.$_SESSION['currId'].'</pre></div>';
-echo '<div class="desc"><pre>'.$_SESSION["currPic"].'</pre></div>';
+  // echo '<div class="desc"><pre>'.$_SESSION['currId'].'</pre></div>';
+// echo '<div class="desc"><pre>'.$_SESSION["currPic"].'</pre></div>';
   echo '<div class="desc"><pre>'.print_r($picutre[0],1).'</pre></div>';
     // echo '<div class="desc">id= '.$id.'</div>';
     echo '<div class="desc">'.
